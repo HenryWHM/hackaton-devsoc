b25c623a84db264cd50196c9866014414b003cbd
HenryWHM <whm2903@gmail.com>
2024-10-16 01:28:40 +1100

    Added a toggle button in the Settings page

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const SettingsPage = () => {
+const SettingsPage = ({ theme, toggleTheme }) => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Settings Page
-      </h1>
+    <div className={theme}>
+      <h1 className="text-3xl font-bold underline">Settings Page</h1>
       <button className="btn btn-primary" onClick={() => navigate("/")}>Home</button>
+      <br />
+      <button className="btn btn-primary" onClick={toggleTheme}>
+        {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+      </button>
     </div>
   )
 }
