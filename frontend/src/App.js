64f78d522b5c8d8194f2d7363b02f45766d126bf
Riley Leighton <riley.leighton03@gmail.com>
2024-10-16 20:45:30 +1100

    Added state management with Context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/HomePage';
 import './App.css';
 import SchedulePage from './pages/SchedulePage';
 import SettingsPage from './pages/SettingsPage';
-import { initialValue } from './context';
+import { Context, initialValue } from './context';
 import Loading from './components/Loading';
 
 function App() {
@@ -19,8 +19,11 @@ function App() {
     setters.setLoading(true)
     const localStorageUserData = localStorage.getItem('userData');
     if (localStorageUserData) {
+      console.log("localstoragedata already exists")
       setters.setUserData(JSON.parse(localStorageUserData));
+      console.log("localstoragedata: ", JSON.parse(getters.userData))
     } else {
+      console.log("localstoragedata does not exist")
       // Set default user data if none exists in local storage
       let defaultUserData = {
         tasks: [],
@@ -32,36 +35,22 @@ function App() {
     setters.setLoading(false);
   }, []);
 
-  useEffect(() => {
-    // Handle light/dark mode
-    if (userData.mode === 'dark') {
-      document.documentElement.classList.add('dark');
-      document.documentElement.classList.remove('light');
-    } else {
-      // Default is light mode if not saved theme
-      document.documentElement.classList.add('light');
-      document.documentElement.classList.remove('dark');
-    }
-  }, [userData.mode]);
-
   if (getters.isLoading) {
     return <Loading />;
   }
-  const toggleMode = () => {
-    setUserData((prevUserData) => ({ ...prevUserData, mode: prevUserData.mode === 'light' ? 'dark' : 'light' }));
-    // setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-  };
 
   return (
-    <div className={`min-h-screen ${userData.mode === 'dark' ? 'bg-darkBackground text-lightText' : 'bg-lightBackground text-darkText'}`}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage mode={userData.mode} />} />
-          <Route path="/schedule" element={<SchedulePage mode={userData.mode} />} />
-          <Route path="/settings" element={<SettingsPage mode={userData.mode} toggleMode={toggleMode} />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Context.Provider value={{ getters, setters }}>
+      <div className={`min-h-screen ${userData.mode === 'dark' ? 'bg-darkBackground text-lightText' : 'bg-lightBackground text-darkText'}`}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/schedule" element={<SchedulePage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Context.Provider>
   );
 }
 
--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Context, useContext } from '../context';
 
-const SettingsPage = ({ mode, toggleMode }) => {
+const SettingsPage = () => {
+  const { getters, setters } = useContext(Context);
   const navigate = useNavigate();
 
+  const toggleMode = () => {
+    setters.setUserData((prevUserData) => ({ ...prevUserData, mode: prevUserData.mode === 'light' ? 'dark' : 'light' }));
+  };
+
   return (
-    <div className={`min-h-screen ${mode === 'dark' ? 'bg-darkBackground text-lightText' : 'bg-lightBackground text-darkText'}`}>
-      <h1 className={`text-3xl font-bold underline ${mode === 'dark' ? 'text-lightText' : 'text-darkText'}`}>Settings Page</h1>
+    <div className={`min-h-screen ${getters.userData.mode === 'dark' ? 'bg-darkBackground text-lightText' : 'bg-lightBackground text-darkText'}`}>
+      <h1 className={`text-3xl font-bold underline ${getters.userData.mode === 'dark' ? 'text-lightText' : 'text-darkText'}`}>Settings Page</h1>
       <button className="mt-4 p-2 bg-blue-500 text-white dark:bg-darkAccent rounded" onClick={() => navigate("/")}>Home</button>
       <br />
       <button className="mt-4 p-2 bg-blue-500 text-white dark:bg-darkAccent rounded" onClick={toggleMode}>
-        {mode === 'light' ? 'Dark Mode' : 'Light Mode'}
+        {getters.userData.mode === 'light' ? 'Dark Mode' : 'Light Mode'}
       </button>
     </div>
   )
